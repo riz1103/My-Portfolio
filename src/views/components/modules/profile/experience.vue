13e8dee3a77fc00b05528f8acd5ff5572d23b858
Riz <joserizc@gmail.com>
2023-05-17 08:54:06 +0800

    Fix

--- src/views/components/modules/profile/experience.vue
+++ src/views/components/modules/profile/experience.vue
@@ -5,10 +5,9 @@
             <div><b>{{`${experience.company_name}, ${experience.company_address}`}}</b></div>
             <div>{{experience.date}}</div>
             <div>{{experience.job_description}}</div>
-            <div class="projects" style="margin-left:20px" v-if="experience.project">Projects:</div> 
-            <li v-for="proj in experience.project" :key="proj.name" style="margin-left:20px">
-                <a :href="proj.urlString"><b>{{proj.name}}</b></a><p style="margin-left:20px">{{proj.description}}</p>
-                <p v-if="proj.skill_used" style="margin-left:20px"><b>Skills Applied: <span class="blink">{{proj.skill_used}}</span></b></p>
+            <div class="projects" v-if="experience.project">Projects:</div> 
+            <li v-for="proj in experience.project" :key="proj.name">
+                <a :href="proj.urlString">{{proj.name}}</a> 
             </li>
         </div>
     </div>
@@ -19,55 +18,27 @@ export default {
         return {
             exp:[
                 {
-                    company_name: 'Booth & Partners Philippines Inc',
-                    company_address: 'Makati City',
-                    date: 'November 2021 – Present',
-                    position: 'Frontend Developer',
-                    job_description: 'Working as a front-end developer for an Australian client (Australian ISP) customer and internal portal for managing customer accounts and payments. Also doing some modifications and development in Microsoft Dynamics CRM as well as creating my own custom components in it using React js.',
-                    project:[
-                        {
-                            name:'SpiritX Version 2',
-                            urlString:'https://v2.spiritx.com.au/',
-                            description:'A customer portal and a support management app for an Australian ISP Company.',
-                            skill_used:'Vue js (Nuxt)'
-                        },
-                        {
-                            name:'Microsoft Dynamics CRM Projects',
-                            description: 'Modification of existing components and creating my own custom controls for it to enhance the look and the flexibility.',
-                            skill_used:'React js and MS Dynamics knowledge'
-                        }                     
-                    ]
-                },
-                {
                     company_name: 'Yondu Inc',
                     company_address: 'BGC Makati City',
-                    date: 'October 2020 – November 2021',
-                    position: 'Senior Software Engineer',
+                    date: 'October 2020 – Present',
+                    position: 'Vue Developer',
                     job_description: 'Working as a front-end developer in ecommerce app and content management systems through agile scrum. Creates and implements reusable components for more effective and faster development for the team depending on the requirements of the client.',
                     project:[
                         {
                             name:'REX Content Management System',
-                            urlString:'http://54.255.43.137:81/',
-                            description:'A Content Management System web app to manage the contents in the Rex Storefront.',
-                            skill_used:'Vue js (Nuxt js)'
+                            urlString:'http://54.255.43.137:81/'
                         },
                         {
                             name:'REX Storefront',
-                            urlString:'https://stg-estore-sf.yondu.net/',
-                            description:'An e-commerce web app for a bookstore.',
-                            skill_used:'Vue js (Quasar)'
+                            urlString:'https://stg-estore-sf.yondu.net/'
                         },
                         {
                             name:'WEE Content Management System',
-                            urlString:'https://wee-trial.yondu.com/',
-                            description:'A Content Management System web app to manage the contents in the WEE Storefront.',
-                            skill_used:'Vue js (Nuxt js)'
+                            urlString:'https://wee-trial.yondu.com/'
                         },
                         {
                             name:'WEE Storefront',
-                            urlString:'https://wee-trial.storefront.yondu.com/',
-                            description:'An e-commerce web app.',
-                            skill_used:'Vue js (Nuxt js)'
+                            urlString:'https://wee-trial.storefront.yondu.com/'
                         }
                         
                     ]
@@ -81,46 +52,34 @@ export default {
                     project:[
                         {
                             name:'FSC Website',
-                            urlString:'https://apps.fastlogistics.com.ph/fscweb/#/',
-                            description:'Company\'s official commercial website. Also act as an employee portal to access employees information and tasks including the status of those. This includes the "Ladderized Report" automation.',
-                            skill_used: 'Vue js for frontend, and C#.NET for the backend.'
+                            urlString:'https://apps.fastlogistics.com.ph/fscweb/#/'
                         },
                         {
                             name:'Aquila Go',
-                            urlString:'https://aquilago.fastlogistics.com.ph/',
-                            description:'Warehouse Management System web app and mobile.',
-                            skill_used: 'Vue js for frontend, C#.NET for the backend, and Capacitor for compilation into mobile.'
+                            urlString:'https://aquilago.fastlogistics.com.ph/'
                         },
                         {
                             name:'Itsek Web',
-                            urlString:'https://itsek.fastlogistics.com.ph/',
-                            description:'A warehouse checklist web app.',
-                            skill_used: 'Vue js for frontend, C#.NET for the backend, and Capacitor for compilation into mobile.'
+                            urlString:'https://itsek.fastlogistics.com.ph/'
                         },
                         {
                             name:'Itsek Mobile',
-                            urlString:'https://play.google.com/store/apps/details?id=itsektwo.fastgroup.biz',
-                            description:'A warehouse checklist app for mobile devices.',
-                            skill_used: 'Vue js for frontend, C#.NET for the backend, and Capacitor for compilation into mobile.'
+                            urlString:'https://play.google.com/store/apps/details?id=itsektwo.fastgroup.biz'
                         },
                         {
                             name:'Vendors Accreditation Systems Online (VASO)',
-                            urlString:'https://apps.fastlogistics.com.ph/vaso/#/login',
-                            description:'A portal for all vendors/suppliers that they are using to apply as an accredited vendor/supplier for the company.',
-                            skill_used: 'Vue js for frontend, and C#.NET for the backend.'
+                            urlString:'https://apps.fastlogistics.com.ph/vaso/#/login'
                         },
                         {
                             name:'Quality Management System (QMS)',
-                            urlString:'https://apps.fastlogistics.com.ph/qms/#/',
-                            description:'A report automation web app focused on automatically generating reports with analysis attached pertaining to quality aspect of the workplace.',
-                            skill_used: 'Vue js for frontend, and C#.NET for the backend.'
+                            urlString:'https://apps.fastlogistics.com.ph/qms/#/'
                         },
                     ]
                 },
                 {
                     company_name: 'Enigma Technologies Inc. ',
                     company_address: 'Sta. Rosa, Laguna',
-                    date: 'October 2020 – Present',
+                    date: 'January 2010 – February 2011',
                     position: 'Computer Technician',
                     job_description: 'Assembles, configure and troubleshoot computers both laptop and desktop. Network cabling and CCTV installation.'
                 },
@@ -130,58 +89,13 @@ export default {
 }
 </script>
 <style scoped>
-.itemized{
+/* .itemized{
     margin:20px;
-}
+} */
 .my-wrapper{
     margin-top:-30px;
 }
 .projects{
     font-weight: bold;
-    margin-top:10px;
-    margin-bottom: 5px;
-}
-.blink{
-  animation: blink 5s infinite;
-}
-@keyframes blink{
-  0% {
-    color: red;
-  }
-  20% {
-    color: green;
-  }
-  40% {
-    color: yellow;
-  }
-  60% {
-    color: blue;
-  }
-  80% {
-    color: orange;
-  }
-  100% {
-    color: red;
-  }
-}
-@-webkit-keyframes blink{
-  0% {
-    color: red;
-  }
-  20% {
-    color: green;
-  }
-  40% {
-    color: yellow;
-  }
-  60% {
-    color: blue;
-  }
-  80% {
-    color: orange;
-  }
-  100% {
-    color: red;
-  }
 }
 </style>
